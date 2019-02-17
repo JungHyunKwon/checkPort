16e6e362888fe9840ae744625ca5dac775facf8e
JungHyunKwon <kjh_3859@naver.com>
2019-02-17 18:57:50 +0900

    published

--- checkPort.js
+++ checkPort.js
@@ -10,22 +10,28 @@ const net = require('net');
 
 /**
  * @param {number} port
- * @param {function} callback
- * @return {boolean}
+ * @param {function} callback {boolean}
  */
 module.exports = (port, callback) => {
-	//함수일 때
-	if(typeof callback === 'function') {
-		let server = net.createServer();
+	//숫자일 때
+	if(typeof port === 'number' && !isNaN(port) && isFinite(port)) { 
+		//함수일 때
+		if(typeof callback === 'function') {
+			let server = net.createServer();
 
-		server.once('listening', () => {
-			server.once('close', () => {
-				callback(false);
-			}).close();	
-		}).once('error', (error) => {
-			callback(true);
-		});
+			server.once('listening', () => {
+				server.once('close', () => {
+					callback(false);
+				}).close();	
+			}).once('error', (error) => {
+				callback(true);
+			});
 
-		server.listen(port);
+			server.listen(port);
+		}else{
+			console.error('callback : 함수가 아닙니다.');
+		}
+	}else{
+		console.error('port : 숫자가 아닙니다.');
 	}
 };
